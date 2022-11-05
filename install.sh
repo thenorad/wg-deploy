f81a6066e241f3f6bf9eab86d7fa1e76f5c3536f
thenorad <rostik.vorobev@ya.ru>
2022-11-05 16:47:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #update system and install packages
 sudo apt update
+if [ ! $? -eq 0 ]; then echo 'ERROR: cant update APT cache, try to make it itself: sudo apt update'; exit 1; fi
 sudo apt -y upgrade
+if [ ! $? -eq 0 ]; then echo 'ERROR: cant upgrade system, try to make it itself: sudo apt upgrade'; exit 1; fi
 sudo apt -y install wireguard nftables jq unzip
+if [ ! $? -eq 0 ]; then echo 'ERROR: cant download packages, try to make it itself: apt install wireguard nftables jq unzip'; exit 1; fi
 
 #prepare DB and wireguard binary
 unzip db.zip
